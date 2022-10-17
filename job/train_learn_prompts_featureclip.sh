e3a14628422591c84e7eb972bd0e49d5e2281c23
hancong11 <hancong11@meituan.com>
2022-10-17 11:24:38 +0800

    test prompt

--- job/train_learn_prompts_featureclip.sh
+++ job/train_learn_prompts_featureclip.sh
@@ -1,6 +1,6 @@
 # Training prompts
 export DETECTRON2_DATASETS=/mnt/dolphinfs/hdd_pool/docker/user/hadoop-vacv/hancong/code/pretrain/ov-seg/zsseg.baseline/datasets
-outputDir="/mnt/dolphinfs/hdd_pool/docker/user/hadoop-vacv/hancong/code/pretrain/ov-seg/zsseg.baseline/output/promptproposal/learnprompt_featureclip_bs32_10k_4"
+outputDir="/mnt/dolphinfs/hdd_pool/docker/user/hadoop-vacv/hancong/code/pretrain/ov-seg/zsseg.baseline/output/promptproposal/learnprompt_featureclip_bs32_10k"
 # CUDA_VISIBLE_DEVICES=1 python3 /mnt/dolphinfs/hdd_pool/docker/user/hadoop-vacv/hancong/code/pretrain/ov-seg/zsseg.baseline/train_net.py --config-file /mnt/dolphinfs/hdd_pool/docker/user/hadoop-vacv/hancong/code/pretrain/ov-seg/zsseg.baseline/configs/coco-stuff-164k-156/zero_shot_proposal_classification_learn_prompt_bs32_10k_featureclip.yaml --num-gpus 1 OUTPUT_DIR outputDir
 python3 /mnt/dolphinfs/hdd_pool/docker/user/hadoop-vacv/hancong/code/pretrain/ov-seg/zsseg.baseline/train_net.py --config-file /mnt/dolphinfs/hdd_pool/docker/user/hadoop-vacv/hancong/code/pretrain/ov-seg/zsseg.baseline/configs/coco-stuff-164k-156/zero_shot_proposal_classification_learn_prompt_bs32_10k_featureclip.yaml --num-gpus 4 --eval-only --resume OUTPUT_DIR $outputDir
 # Training seg model
